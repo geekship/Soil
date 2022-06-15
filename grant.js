c313b401d48d2e563f6486ff1a4e3351f43ce6fc
martin vavrovic <martin.vavrovic@gmail.com>
2022-06-15 20:30:42 +0200

    add gitcoin grant draft

---
+++ grant.js
@@ -0,0 +1,9 @@
+export default {
+  title: 'Scalable Open Innovation Ledger',
+  description: `An open Ideation platform that realigns incentives between innovators, academia and business. 
+`,
+  tags: ['*Crypto Advocacy', '*Web3', 'Social Identity', 'DAOs'],
+  github: 'https://github.com/geekship/soil',
+  website: 'https://geekship.org',
+  twitter: 'SoilLedger',
+}
